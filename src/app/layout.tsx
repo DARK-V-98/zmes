bc03aa4efad28f15d1fb5e9b0abaf1eb963c4dca
Thimira Vishwa <vishwathimira2@gmail.com>
2025-09-13 02:01:28 +0000

    some emojies like 😫 dont show in the webapp thety show like 🫣🫣 this

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta charSet="utf-8" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
